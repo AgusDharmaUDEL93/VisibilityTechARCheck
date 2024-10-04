5c1b75a84f61290e7ee404173ef935bc1d1be843
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-10-04 09:21:08 +0700

    Initial Commit

---
+++ VisibilityTechARCheck/VisibilityTechARCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisibilityTechARCheckApp.swift
+//  VisibilityTechARCheck
+//
+//  Created by Putu Agus Dharma Kusuma on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VisibilityTechARCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisibilityTechARCheckTests/VisibilityTechARCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisibilityTechARCheckTests.swift
+//  VisibilityTechARCheckTests
+//
+//  Created by Putu Agus Dharma Kusuma on 04/10/24.
+//
+
+import Testing
+@testable import VisibilityTechARCheck
+
+struct VisibilityTechARCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
